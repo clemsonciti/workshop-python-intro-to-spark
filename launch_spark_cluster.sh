e033aef6f6cae66d2f0df74dce760ad6dd2cf297
Jeffrey Denton <DENTON@clemson.edu>
2020-05-08 13:04:38 -0400

    Minor changes to launch_spark_cluster.sh to leverage PBS_ environment variables rather than grep on the job name jupyterhub. Also corrected a minor error in the number of workers being reported back to the user.

--- launch_spark_cluster.sh
+++ launch_spark_cluster.sh
@@ -4,21 +4,21 @@ export SPARK_HOME="/home/$USER/software/spark-2.4.5-bin-hadoop2.7"
 export SPARK_CONF_DIR=$SPARK_HOME/conf
 export PATH=$SPARK_HOME:$PATH
 
-master=`cat /var/spool/pbs/aux/$(qstat -anu $USER | grep jupyterhub | awk 'END {print $1}') | uniq | head -n 1`
+master=`cat $PBS_NODEFILE | sort | uniq | head -n 1`
 echo $master > $SPARK_HOME/conf/master
 
-cat /var/spool/pbs/aux/$(qstat -u $USER | tail -n 1 | awk 'END {print $1}') | uniq | tail -n +2 > $SPARK_HOME/conf/slaves
+cat $PBS_NODEFILE | sort | uniq | tail -n +2 > $SPARK_HOME/conf/slaves
 
 # get num node info
-N=`qstat -anu $USER | grep jupyterhub | awk 'END {print $6}'`
+N=`qstat -an1 $PBS_JOBID | awk 'END {print $6}'`
 
 # get per-node memory info
-totalRAMtxt=`qstat -anu $USER | grep jupyterhub | awk 'END {print $8}'`
-totalRAMnum=${totalRAMtxt%??}; 
+totalRAMtxt=`qstat -an1 $PBS_JOBID | awk 'END {print $8}'`
+totalRAMnum=${totalRAMtxt%??}
 oneRAM=`expr $totalRAMnum / $N - 1`G
 
 # get per-node core info
-totalCORESnum=`qstat -anu $USER | grep jupyterhub | awk 'END {print $7}'`
+totalCORESnum=`qstat -an1 $PBS_JOBID | awk 'END {print $7}'`
 oneCORES=`expr $totalCORESnum / $N`
 
 # get num workers
@@ -32,9 +32,8 @@ cat $SPARK_HOME/conf/slaves
 echo "Temporary dir:" $TMPDIR
 echo "Memory per worker (GB):" $oneRAM
 echo "Cores per worker:" $oneCORES
-echo "Num workers:" $N
+echo "Num workers:" $workers
 
 command=". $SPARK_HOME/sbin/start-all.sh -h $master -d $TMPDIR -m ${oneRAM} -c $workers"
 echo $command
 $command
-
