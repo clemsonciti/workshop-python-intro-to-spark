448279e673e6438e606c4b7be62a9505432ef957
Linh Ngo <lngo@clemson.edu>
2020-09-01 19:38:30 -0400

    minor fix

--- download_spark.sh
+++ download_spark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget http://www.gtlib.gatech.edu/pub/apache/spark/spark-2.4.5/spark-2.4.5-bin-hadoop2.7.tgz
+cp /zfs/citi/spark/spark-2.4.5-bin-hadoop2.7.tgz .
 mkdir -p ~/software/
 tar -xzf spark-2.4.5-bin-hadoop2.7.tgz -C ~/software/
 rm spark-2.4.5-bin-hadoop2.7.tgz
